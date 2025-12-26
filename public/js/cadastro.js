c6c6ca15f52e832698f5d926a00f85ced68e009b
Alexandre Perissinotto <alexandrepamaral@gmail.com>
2025-12-26 19:05:35 -0300

    cep ok

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -28,6 +28,11 @@ document.addEventListener('DOMContentLoaded', () => {
         mensagem.innerHTML = 'Digite o seu nome completo';
     });
 
+    // Quando o campo de e-mail recebe foco (clique), exibe uma mensagem de ajuda
+    inputEmail.addEventListener('focus', () => {
+        mensagem.innerHTML = 'Digite o seu e-mail no formato email@exemplo.com.br';
+    });
+
     // Quando o campo de data de nascimento recebe foco, exibe uma mensagem de ajuda
     inputDataNascimento.addEventListener('focus', () => {
         mensagem.innerHTML = 'Digite a sua data de nascimento com DD/MM/AAAA';
@@ -45,27 +50,109 @@ document.addEventListener('DOMContentLoaded', () => {
         inputDataNascimento.value = value;
     });
 
+    // Quando o campo de cpf recebe foco (clique), exibe uma mensagem de ajuda
+    inputCpf.addEventListener('focus', () => {
+        if (inputCpf.value === "")
+            mensagem.innerHTML = 'Digite o seu CPF no formato 999.999.999-99';
+        else
+            mensagem.innerHTML = 'Cpf inválido';
+    });
+
+    // Quando o campo de cpf recebe dados (input), valida o contéudo e exibe uma mensagem de ajuda
+    inputCpf.addEventListener('input', () => {
+        let value = inputCpf.value.replace(/\D/g, ""); // remove tudo que não for número
+
+        value = value.replace(/(\d{3})(\d)/, "$1.$2");
+        value = value.replace(/(\d{3})(\d)/, "$1.$2");
+        value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+
+        inputCpf.value = value;
+    });
+
+    inputCpf.addEventListener('blur', () => {
+        if (!validarCPF(inputCpf.value)) {
+            inputCpf.focus();
+            return;
+        }
+    });
+
+    // Quando o campo de rg recebe foco (clique), exibe uma mensagem de ajuda
+    inputRg.addEventListener('focus', () => {
+        if (inputRg.value === "")
+            mensagem.innerHTML = 'Digite o seu RG no formato 99.999.999-9';
+        else
+            mensagem.innerHTML = 'RG inválido';
+    });
+
+    // Quando o campo de rg recebe dados (input), valida o contéudo e exibe uma mensagem de ajuda
+    inputRg.addEventListener('input', () => {
+        let value = inputRg.value.replace(/[^0-9Xx]/g, "").toUpperCase();
+
+        if (value.length > 2)
+            value = value.replace(/^(\d{2})(\d)/, "$1.$2");
+
+        if (value.length > 6)
+            value = value.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+
+        if (value.length > 9)
+            value = value.replace(/^(\d{2})\.(\d{3})\.(\d{3})([0-9X])$/, "$1.$2.$3-$4");
+
+        inputRg.value = value;
+    });
+
+    inputRg.addEventListener('blur', () => {
+        const regex = /^\d{2}\.\d{3}\.\d{3}-[0-9Xx]$/;
+        if (!regex.test(inputRg.value)) {
+            inputRg.focus();
+            return;
+        }
+    });
+
     // Quando o campo de cep recebe foco, exibe uma mensagem de ajuda
     inputCep.addEventListener('focus', () => {
-        mensagem.innerHTML = 'Digite o cep da sua rua';
+        if (inputCep.value === "")
+            mensagem.innerHTML = 'Digite o cep do seu endereço';
+        else
+            mensagem.innerHTML = 'Cep inválido, ou inexistente!';
     });
 
     // Quando o campo de cep recebe dados (input), valida o contéudo e exibe uma mensagem de ajuda
     inputCep.addEventListener('input', () => {
+        let value = inputCep.value.replace(/\D/g, ""); // remove tudo que não for número
+        inputCep.value = value;
         const regex = /^\d{8}$/;
         if (!regex.test(inputCep.value)) {
-            mensagem.innerHTML = 'Cep inválido, pode conter apenas números';
+            mensagem.innerHTML = 'Cep inválido, deve conter 8 números';
+            return;
         } else {
             mensagem.innerHTML = 'Digite o cep da sua rua';
-            fetch(`https://viacep.com.br/ws/${inputCep.value}/json/`)
-                .then((response) => response.json())
-                .then((json) => {
+        }
+    });
+
+    inputCep.addEventListener('blur', () => {
+        fetch(`https://viacep.com.br/ws/${inputCep.value}/json/`)
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((json) => {
+                console.log(json);
+                if (!json.erro) {
                     inputEndereco.value = json.logradouro;
                     inputBairro.value = json.bairro;
                     inputCidade.value = json.localidade;
                     inputEstado.value = json.estado;
-                })
-        }
+                } else {
+                    mensagem.innerHTML = 'Cep inválido, ou inexistente!';
+                    inputCep.focus();
+                    return;
+                }
+            })
+            .catch((erro) => {
+                mensagem.innerHTML = 'Cep inválido, ou inexistente!';
+                inputCep.focus();
+                return;
+            });
     });
 
     // Quando o formulário é enviado (botão clicado)
@@ -121,3 +208,28 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+function validarCPF(cpf) {
+    cpf = cpf.replace(/\D/g, '');
+
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+    let soma = 0;
+    let resto;
+
+    for (let i = 1; i <= 9; i++)
+        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.substring(9, 10))) return false;
+
+    soma = 0;
+    for (let i = 1; i <= 10; i++)
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+
+    return resto === parseInt(cpf.substring(10, 11));
+}

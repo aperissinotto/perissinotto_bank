4d6edd53e0127828ab019c479e8a9ce2b0cc1bea
Alexandre Perissinotto <alexandrepamaral@gmail.com>
2025-12-30 20:30:47 -0300

    correções no db

--- backend/db/db.go
+++ backend/db/db.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -12,7 +14,17 @@ var DB *sql.DB
 func ConectarPostgres() {
 	var err error
 
-	dsn := "postgres://postgres:admin123@localhost:5432/postgres?sslmode=disable"
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
+	//dsn := "postgres://postgres:admin123@localhost:5432/postgres?sslmode=disable"
 
 	DB, err = sql.Open("pgx", dsn)
 	if err != nil {
--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aperissinotto/perissinotto_bank/db"
 	"github.com/aperissinotto/perissinotto_bank/handlers"
 )
 
 func main() {
-	//	db.ConectarPostgres()
-	//	defer db.DB.Close()
+	db.ConectarPostgres()
+	defer db.DB.Close()
 
 	// Rotas de API
 	http.HandleFunc("/api/login", handlers.HandleLogin)

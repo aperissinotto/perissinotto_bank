1244b69c47b1ab219bb976d63b47d25c3749ce7f
Alexandre Perissinotto <alexandrepamaral@gmail.com>
2026-01-03 13:10:40 -0300

    correções no cadastro

--- frontend/js/cadastro.js
+++ frontend/js/cadastro.js
@@ -294,12 +294,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
             mensagem.innerHTML = 'Validando dados informados no Mainframe...';
 
-            // Aguarda e converte a resposta do servidor em JSON
-            console.log(response);
-            const result = await response.json();
             // Se a requisição foi bem-sucedida (código 200, 201, etc)
             if (response.ok) {
-                mensagem.innerHTML = 'Login realizado com sucesso!';
+                mensagem.innerHTML = 'Cadastro realizado com sucesso!';
             } else {
                 // Se houve erro, exibe a mensagem de erro retornada pelo servidor
                 mensagem.innerHTML = `Erro: ${result.message || 'Login falhou'}`;
